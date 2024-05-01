37ea43eae982078d78c38a1148bd4063515c2cff
Nadia <nmarenkova03@gmail.com>
2024-05-01 12:15:59 +0300

    When we create student or instructor they are saved to the database, but, they are not displayed on the page. If we use search bar to find a student or instructor we have just added it will be found.

--- ContosoUniversity/Controllers/InstructorsController.cs
+++ ContosoUniversity/Controllers/InstructorsController.cs
@@ -90,7 +90,7 @@ namespace ContosoUniversity.Controllers
         }
 
         // POST: Instructors/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
